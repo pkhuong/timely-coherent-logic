067c066dd0a0bf1818d936895ed691f8c3c9559c
Paul Khuong <pvk@pvk.ca>
2020-12-28 17:33:52 -0500

    unification/metavariable: matching variables
    This is how we'll pair what values (variable) goes with what pattern
    variable (metevariable).
    
    Metavariable are also important for join ordering: they represent
    attributes for equijoining (or antijoins).  We'll make it easy to
    do something not too horrible with linearly sequential allocation
    ids: metavariables that are defined earlier in clauses will compare
    lesser, and should probably be joined later.
    
    There also shouldn't be too many of them, and metavariables shouldn't
    be Clone()d in inner loops, so we annotate each one with a string
    description (which is only used for pretty printing).

---
+++ timely-coherent-propagator/src/unification/metavariable.rs
@@ -0,0 +1,100 @@
+/// A metavariable is a name for, e.g., a bound variable in $$\forall x$$.
+///
+/// Metavariables are uniquely identified by their sequence id; the
+/// name itself is only useful for pretty-printing.
+///
+/// The implicit order on metavariables sorts by sequence id
+/// (ascending).  This order should naturally do what one expects for
+/// hierarchies, as long as metavariables are generated with a
+/// pre-order depth-first traversal.
+#[derive(Clone, Debug, Eq)]
+pub struct MetaVar {
+    sequence: usize,
+    name: String,
+}
+
+impl MetaVar {
+    /// Returns a new `MetaVar` with `name`.
+    #[must_use]
+    pub fn new(name: &str) -> Self {
+        #[cfg(not(tarpaulin_include))]
+        fn id() -> usize {
+            use std::sync::atomic::{AtomicUsize, Ordering};
+            static METAVAR_COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+            METAVAR_COUNTER.fetch_add(1, Ordering::Relaxed)
+        }
+
+        Self {
+            sequence: id(),
+            name: name.into(),
+        }
+    }
+}
+
+impl std::hash::Hash for MetaVar {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.sequence.hash(state);
+    }
+}
+
+impl PartialEq for MetaVar {
+    fn eq(&self, other: &Self) -> bool {
+        self.sequence == other.sequence
+    }
+}
+
+impl PartialOrd for MetaVar {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for MetaVar {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.sequence.cmp(&other.sequence)
+    }
+}
+
+#[test]
+fn test_smoke() {
+    let mv0 = MetaVar::new("zxc");
+    let mv1 = MetaVar::new("asd");
+
+    assert!(mv0 < mv1);
+    assert_ne!(mv0, mv1);
+}
+
+#[test]
+fn test_eq_hash() {
+    use std::collections::hash_map::DefaultHasher;
+    use std::hash::Hash;
+    use std::hash::Hasher;
+
+    let mv0 = MetaVar::new("a");
+    let mv0_clone = mv0.clone();
+    // mv0 and mv1 should be distinct, despite their name.
+    let mv1 = MetaVar::new("a");
+
+    assert_eq!(mv0, mv0_clone);
+    assert_ne!(mv0, mv1);
+
+    {
+        let mut h0 = DefaultHasher::new();
+        let mut h1 = DefaultHasher::new();
+
+        mv0.hash(&mut h0);
+        mv1.hash(&mut h1);
+        assert_ne!(h0.finish(), h1.finish());
+    }
+
+    {
+        let mut h0 = DefaultHasher::new();
+        let mut h0_clone = DefaultHasher::new();
+
+        mv0.hash(&mut h0);
+        mv0_clone.hash(&mut h0_clone);
+
+        assert_eq!(h0.finish(), h0_clone.finish());
+    }
+}
