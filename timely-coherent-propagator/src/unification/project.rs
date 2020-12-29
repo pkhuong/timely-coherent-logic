cfba963d454d6bb03f2bd686e844e2d964a6ee73
Paul Khuong <pvk@pvk.ca>
2020-12-28 19:06:51 -0500

    unification/project: project and reorder captures
    This will be useful to compute join keys, and to drop matches that are
    only used in join conditions, but not used in any result template.

---
+++ timely-coherent-propagator/src/unification/project.rs
@@ -0,0 +1,119 @@
+//! As part of the join logic, we must restructure matches by removing
+//! or re-ordering metavariables, and applying the same transformation
+//! to individual captures (lists of variables).
+use super::MetaVar;
+use crate::ground::{Capture, Variable};
+use std::convert::TryFrom;
+
+/// A Projection takes a single capture (with shape `input`), and
+/// returns an output of shape `output`.
+pub struct Projection {
+    input: Vec<MetaVar>,
+    output: Vec<MetaVar>,
+    fun: Box<dyn Fn(&Capture) -> Capture>,
+}
+
+impl Projection {
+    /// Returns a Projection from `inp` into `out`.  This function may fail
+    /// if `out` refers to a metavariable absent from `inp`.
+    ///
+    /// # Errors
+    ///
+    /// Will return `Err` if `out` includes a `MetaVar` absent from `inp`.
+    pub fn new(inp: &[MetaVar], out: &[MetaVar]) -> Result<Projection, &'static str> {
+        make_projection(inp, out)
+    }
+
+    #[must_use]
+    pub fn input(&self) -> &[MetaVar] {
+        &self.input
+    }
+
+    #[must_use]
+    pub fn output(&self) -> &[MetaVar] {
+        &self.output
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn apply(&self, input: &Capture) -> Capture {
+        (self.fun)(input)
+    }
+}
+
+fn make_projection(inp: &[MetaVar], out: &[MetaVar]) -> Result<Projection, &'static str> {
+    let find_index = |needle: &MetaVar| {
+        for (index, haystack) in inp.iter().enumerate() {
+            if haystack == needle {
+                return Ok(index);
+            }
+        }
+
+        Err("Needle not found in the input haystack")
+    };
+
+    // The `indices` vector tells us where to find each output value
+    // in the input `Capture`: the first value is the index of the
+    // first output, second value that of the second output, etc.
+    // Store these indices in a vector of u8: we don't expect very
+    // wide fact tables.
+    let mut indices = Vec::<u8>::with_capacity(inp.len());
+    for needle in out.iter() {
+        let index = find_index(needle)?;
+        indices.push(u8::try_from(index).expect("Wide predicates not supported"));
+    }
+
+    let expected_input_len = inp.len();
+    let output_len = indices.len();
+    let projector = move |capture: &Capture| {
+        let vars = capture.vars();
+        let mut result = Vec::<Variable>::with_capacity(output_len);
+
+        assert_eq!(vars.len(), expected_input_len);
+        for index in indices.iter().cloned() {
+            result.push(vars[index as usize]);
+        }
+
+        Capture::from_vec(result)
+    };
+
+    Ok(Projection {
+        input: inp.into(),
+        output: out.into(),
+        fun: Box::new(projector),
+    })
+}
+
+#[test]
+fn test_project_happy_path() {
+    let x = MetaVar::new("x");
+    let y = MetaVar::new("y");
+    let z = MetaVar::new("z");
+    let input = vec![x.clone(), y.clone(), z.clone()];
+    let output = vec![z, y];
+
+    let projection = Projection::new(&input, &output).expect("ok");
+    assert_eq!(projection.input(), input);
+    assert_eq!(projection.output(), output);
+    assert_eq!(
+        projection.apply(&[Variable::new(1), Variable::new(2), Variable::new(3)].into()),
+        vec![Variable::new(3), Variable::new(2)].into()
+    );
+
+    // Apply the projection multiple times.
+    assert_eq!(
+        projection.apply(&[Variable::new(2), Variable::new(3), Variable::new(4)].into()),
+        vec![Variable::new(4), Variable::new(3)].into()
+    );
+}
+
+#[test]
+fn test_project_missing_variable() {
+    let x = MetaVar::new("x");
+    let y = MetaVar::new("y");
+    let z = MetaVar::new("z");
+    let input = vec![x.clone(), y.clone()];
+    let output = vec![z, y];
+
+    assert!(Projection::new(&input, &output).is_err());
+}
