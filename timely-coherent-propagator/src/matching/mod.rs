de0a718b256fad95b383b81b165cda6b107658d9
Paul Khuong <pvk@pvk.ca>
2020-12-29 18:43:44 -0500

    .## matching: pattern matching in bulk
    We know how to check if a `Fact` tuple matches a pattern, and how to
    extract a `Capture` as a witness of that match, if so.  The `matching`
    modules takes this tuple-at-a-time logic, and uses it to find all
    matches in *collections* of `Fact`s.
    
    If we only had to deal with left-hand side antecendents of the form
    `p(x, y)`, this would be a trivial `filter_map` task.  However, we
    wish to handle antecedents that span multiple predicates,
    e.g. $\forall x, y. p(x, y) \wedge q(y, y)$, or even antecedents
    that match against the same predicate multiple times, e.g.,
    $\forall x, y, z. p(x, y) \wedge p(y, z)$.
    
    Finding matches for these more complex antecedents is equivalent to
    executing a (self-)join.  We'll want to separately plan the join, and
    lower a join plan to a differential dataflow expression graph: DD
    plays a lot of type-level tricks to forbid escaping, so we try to do
    work in advance when we can.  The result is easier to test, read, and
    debug.

---
+++ timely-coherent-propagator/src/matching/mod.rs
@@ -0,0 +1,34 @@
+//! The core of a Coherent Logic (CL) solver is pattern matching on
+//! rules of the form $$\forall x, y, z: p(x, y) \wedge q(y, z) \wedge ... =>
+//! ...$$ This work builds on `unification::Pattern` to match against
+//! multiple facts at once.  We will use this pattern matching to find
+//! potential instantiation parameters for the right-hand side of the
+//! $$=>$$ implication, but also to detect when some instantiation are
+//! useless.
+//!
+//! The right-hand side of the implication is a disjunction of
+//! conjunctions; if any of these conjunctions is already true, the
+//! implication has no logical relevance (for all corresponding
+//! parameters).  CL also lets disjunctions on the right-hand side
+//! introduce new variables, with terms like $$\exists a: r(a, x) \wedge
+//! s(x, y),$$ where some metavariables were bound by matching in the
+//! left-hand side, and others introduced by $$\exists,$$ and
+//! substituted with fresh variables when instantiated.
+//!
+//! A key subtlety in CL is that, as long as we can find tuples that
+//! match $$r(a, x) \wedge s(x, y),$$ the existential expression is
+//! already satisfied, and all corresponding instances of the
+//! implications are irrelevant.  Detecting that after the fact
+//! in a relational fashion seems non-trivial; we'll instead match
+//! on all conjunctions in the right-hand-side, and antijoin with
+//! current matches.
+//!
+//! The execution of matching happens in two phases.  We first
+//! generate an expression tree to describes the steps we'll need to
+//! convert Collections of facts (one per predicate) to a Collection
+//! of matches.  Then, we convert that expression tree to a Collection
+//! processing expression.  The indirection seems like spurious
+//! complexity at first, but having a concrete representation for the
+//! matching expression helps introduce rewrites (e.g., local
+//! heuristic transforms or global tree parsing), and is also useful
+//! for debugging.
