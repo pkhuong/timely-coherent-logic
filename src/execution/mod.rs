451a3daf0a3e36fa407c7ebf54a0417e58bc42f6
Paul Khuong <pvk@pvk.ca>
2020-12-31 16:28:33 -0500

    execution/sink: updatable counting bags for collection data
    Differential dataflow collections aren't connections as much as
    virtual pipes to construct dataflow graphs.  In order to get data out
    of them, we must listen for multiplicity updates, and pipe those to a
    an actual concrete dictionary data structure.

--- src/execution/mod.rs
+++ src/execution/mod.rs
@@ -1,7 +1,12 @@
 //! The execution layer briges the narrow gap between our query plans
 //! and differential dataflow's collections.
+mod sink;
 mod split_containers;
 
+pub use sink::CaptureSink;
+pub use sink::CaptureWriter;
+pub use sink::FactSink;
+pub use sink::FactWriter;
 pub use split_containers::CaptureCollection;
 pub use split_containers::CaptureVariable;
 pub use split_containers::FactCollection;
