49cfbbc652961f61f8654ac6d01d0b6ab67373cf
Paul Khuong <pvk@pvk.ca>
2021-01-17 14:58:29 -0500

    kb: add FathomedRegion

--- satumerator/src/kb.rs
+++ satumerator/src/kb.rs
@@ -1,5 +1,6 @@
 //! Traits and types that represent Satumerator's knowledge of the
 //! search space.
+use std::collections::BTreeSet;
 use std::fmt::Debug;
 use std::hash::Hash;
 
@@ -13,3 +14,22 @@ where
 
 // `String`s trivially implement `StateAtom`.
 impl StateAtom for String {}
+
+/// A `FathomedRegion` represents the knowledge that we have fully
+/// explored all states that match `partial_assignment`.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct FathomedRegion<A: StateAtom> {
+    pub partial_assignment: BTreeSet<A>,
+}
+
+impl<A: StateAtom> FathomedRegion<A> {
+    /// Returns a `FathomedRegion` for all the atoms in the iteratable `atoms`.
+    pub fn new<Iter>(atoms: Iter) -> Self
+    where
+        Iter: IntoIterator<Item = A>,
+    {
+        Self {
+            partial_assignment: atoms.into_iter().collect::<BTreeSet<_>>(),
+        }
+    }
+}
