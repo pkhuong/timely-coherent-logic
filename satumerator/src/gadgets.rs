083bdd7fbc060667301163fd48bd546123cfb04c
Paul Khuong <pvk@pvk.ca>
2021-01-17 09:03:29 -0500

    gadgets: add cardinality constraint gadgets

--- satumerator/src/gadgets.rs
+++ satumerator/src/gadgets.rs
@@ -12,6 +12,22 @@ pub fn add_nogood(solver: &mut Solver, nogood: &[Lit]) {
     solver.add_clause(&nogood.iter().map(|x| !*x).collect::<Vec<_>>());
 }
 
+/// Forces at least one of `vars` to be true.
+pub fn add_at_least_one_constraint(solver: &mut Solver, vars: &[Lit]) {
+    solver.add_clause(vars);
+}
+
+/// Forces at most one of `vars` to be true.
+pub fn add_at_most_one_constraint(solver: &mut Solver, vars: &[Lit]) {
+    for (i, x) in vars.iter().enumerate() {
+        for y in vars.split_at(i + 1).1 {
+            // For each pair of variable, both variables can't be
+            // true, i.e., at least one must be false.
+            solver.add_clause(&[!*x, !*y]);
+        }
+    }
+}
+
 #[test]
 fn test_nogood() {
     use cryptominisat::Lbool;
@@ -39,6 +55,116 @@ fn test_nogood() {
             Lbool::False
         } else {
             Lbool::True
+        };
+
+        println!(
+            "values={} assumptions={:?} expected={:?}",
+            values, assumptions, expected
+        );
+        assert_eq!(solver.solve_with_assumptions(&assumptions), expected);
+    }
+}
+
+#[test]
+fn test_add_at_least_one() {
+    use cryptominisat::Lbool;
+
+    let mut solver = Solver::new();
+    let (x, y, z) = (solver.new_var(), solver.new_var(), solver.new_var());
+
+    add_at_least_one_constraint(&mut solver, &[x, y, z]);
+
+    // The constraint set is feasible.
+    assert_eq!(solver.solve(), Lbool::True);
+    // Iterate over the truth value for all 3 variables
+    for values in 0..8 {
+        let x_value = (values & 1) != 0;
+        let y_value = (values & 2) != 0;
+        let z_value = (values & 4) != 0;
+        let assumptions = [
+            Lit::new(x.var(), !x_value).expect("ok"),
+            Lit::new(y.var(), !y_value).expect("ok"),
+            Lit::new(z.var(), !z_value).expect("ok"),
+        ];
+        let expected = if x_value | y_value | z_value {
+            Lbool::True
+        } else {
+            Lbool::False
+        };
+
+        println!(
+            "values={} assumptions={:?} expected={:?}",
+            values, assumptions, expected
+        );
+        assert_eq!(solver.solve_with_assumptions(&assumptions), expected);
+    }
+}
+
+#[test]
+fn test_add_at_most_one() {
+    use cryptominisat::Lbool;
+
+    let mut solver = Solver::new();
+    let (x, y, z) = (solver.new_var(), solver.new_var(), solver.new_var());
+
+    add_at_most_one_constraint(&mut solver, &[x, y, z]);
+
+    // The constraint set is feasible.
+    assert_eq!(solver.solve(), Lbool::True);
+    // Iterate over the truth value for all 3 variables
+    for values in 0..8 {
+        let x_value = (values & 1) != 0;
+        let y_value = (values & 2) != 0;
+        let z_value = (values & 4) != 0;
+        let assumptions = [
+            Lit::new(x.var(), !x_value).expect("ok"),
+            Lit::new(y.var(), !y_value).expect("ok"),
+            Lit::new(z.var(), !z_value).expect("ok"),
+        ];
+        // Should be true if `popcount(values) <= 1`
+        let expected = if (values & (values - 1)) == 0 {
+            Lbool::True
+        } else {
+            Lbool::False
+        };
+
+        println!(
+            "values={} assumptions={:?} expected={:?}",
+            values, assumptions, expected
+        );
+        assert_eq!(solver.solve_with_assumptions(&assumptions), expected);
+    }
+}
+
+#[test]
+fn test_add_exactly_one() {
+    use cryptominisat::Lbool;
+
+    let mut solver = Solver::new();
+    let (x, y, z) = (solver.new_var(), solver.new_var(), solver.new_var());
+
+    // Use a pair of constraints to express that exactly one of
+    // x, y, z must be true.
+    add_at_least_one_constraint(&mut solver, &[x, y, z]);
+    add_at_most_one_constraint(&mut solver, &[x, y, z]);
+
+    // The constraint set is feasible.
+    assert_eq!(solver.solve(), Lbool::True);
+    // Iterate over the truth value for all 3 variables
+    for values in 0..8 {
+        let x_value = (values & 1) != 0;
+        let y_value = (values & 2) != 0;
+        let z_value = (values & 4) != 0;
+        let assumptions = [
+            Lit::new(x.var(), !x_value).expect("ok"),
+            Lit::new(y.var(), !y_value).expect("ok"),
+            Lit::new(z.var(), !z_value).expect("ok"),
+        ];
+        // Should be true if `popcount(values) == 1`
+        let expected = if values != 0 && (values & (values - 1)) == 0 {
+            Lbool::True
+        } else {
+            Lbool::False
         };
 
         println!(
