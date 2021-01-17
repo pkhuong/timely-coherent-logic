381b90d6f1e482f7aecada9c3776fd3b87d5fe8d
Paul Khuong <pvk@pvk.ca>
2021-01-16 22:52:21 -0500

    kb: typed wrappers to represent known facts about the search problem
    The whole Satumerator interface is unfortunately extremely generic;
    we'll figure out a way to make the implementation more concrete
    later, once we have a fingerprint trait.

---
+++ satumerator/src/kb.rs
@@ -0,0 +1,28 @@
+//! Traits and types that represent Satumerator's knowledge of the
+//! search space.
+use std::collections::BTreeSet;
+use std::fmt::Debug;
+use std::hash::Hash;
+
+/// A `StateAtom` is an atomic piece of state, e.g., the decision `x =
+/// true`.
+pub trait StateAtom
+where
+    Self: Clone + Debug + Eq + Hash + PartialOrd + Ord + Sized,
+{
+}
+
+/// A `FathomedRegion` represents the knowledge that, for a given
+/// context (or in all contexts if `None`), we have fully explored all
+/// states that match `partial_assignment`.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct FathomedRegion<A: StateAtom> {
+    pub partial_assignment: BTreeSet<A>,
+}
+
+/// A `ChoiceConstraint` is a set of `StateAtoms` from which any valid
+/// state much pick exactly one option to assert as true.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct ChoiceConstraint<A: StateAtom> {
+    pub options: BTreeSet<A>,
+}
